4a16d8bb5d2cf8509fdaebf358afaae486041ac9
veni <hiimveni@icloud.com>
2025-03-11 14:00:45 -0400

    fgf

--- GameUIViewController.swift
+++ GameUIViewController.swift
@@ -17,12 +17,12 @@ class GameUIViewController: UIViewController, ARSessionDelegate, SCNSceneRendere
     
     func generateBlock(layer: Int, numInLayer: Int,
                        color: UIColor = UIColor.green){
-        let x = layer % 2 != 0 ? 0 : Double(numInLayer) * 0.03 - (0.03 * 2)
-        let z = layer % 2 == 0 ? 0 : Double(numInLayer) * 0.03 - (0.03 * 2)
+        let x = layer % 2 != 0 ? 0 : Double(numInLayer) * thickness - (thickness * 2)
+        let z = layer % 2 == 0 ? 0 : Double(numInLayer) * thickness - (thickness * 2))
         let blockNode = SCNNode(geometry: SCNBox(width: thickness, height: thickness, length: thickness * 3, chamferRadius: 0.002))
         blockNode.geometry?.materials.first?.diffuse.contents = color
         blockNode.eulerAngles = SCNVector3(x: 0, y: layer % 2 == 0 ? 0 : Float.pi / 2, z: 0)
-        blockNode.position = SCNVector3(x: Float(x), y: Float((Double(layer) * 0.03) + (thickness/1)), z: Float(z))
+        blockNode.position = SCNVector3(x: Float(x), y: Float((Double(layer) * thickness) + (thickness/1)), z: Float(z))
         blockNode.name = "block\(layer)l\(numInLayer)"
         blockNode.physicsBody = SCNPhysicsBody(type: .dynamic, shape: nil)
         scene.rootNode.addChildNode(blockNode)
